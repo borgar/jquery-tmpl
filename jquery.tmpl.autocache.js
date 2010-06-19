71653ce01baa518c703612581bc55481abda94d0
Ben Blank <ben.blank@gmail.com>
2010-06-20 01:20:24 +0800

    Tweaked the autocache naming regex.

--- jquery.tmpl.autocache.js
+++ jquery.tmpl.autocache.js
@@ -1,7 +1,7 @@
 (function($){
 	$(document).ready(function() {
 		$("script[type='text/html'],.jquery-tmpl").each(function() {
-			var name = this.id.replace(/^template\W|\Wtemplate$/, "");
+			var name = this.id.replace(/^template[^\da-z]?|[^\da-z]?template$/i, "");
 
 			$.templates[name] = $.tmpl(this.innerHTML);
 		}).remove();
